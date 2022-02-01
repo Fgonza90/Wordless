f5d181272293d29d8d1bd5d13a2264f3e62d6a80
Fernando <fernando.vl87@gmail.com>
2022-02-01 13:11:59 +0100

    feature: nueva letra imprescindible en el diccionario

--- Wordle.py
+++ Wordle.py
@@ -9,7 +9,7 @@ for linea in fichero:
 def eligePalabraDiccionario(nuevoDiccionario):
     a=0; b=0;c=0;d=0;e=0;f=0;g=0;h=0;i=0;j=0;k=0;l=0;m=0;n=0;ñ=0;o=0;p=0;q=0;r=0;s=0;t=0;u=0;v=0;w=0;x=0;y=0;z=0;
     diccionarioValores = dict.fromkeys(nuevoDiccionario)
-    abecedario=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z]
+    abecedario=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z, polla]
     #le asigno a las letras un valor según el número de veces que aparezcan en el diccionario. Una vez por palabra
     for palabra in nuevoDiccionario:
         #le quito a la palabra las letras repetidas
